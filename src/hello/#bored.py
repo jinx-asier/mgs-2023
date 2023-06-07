20bdc979f2eb59ff838d6a3f61bf5f04b6ecac17
Jinx Asier <austintalisa@gmail.com>
2023-06-07 16:31:16 +0000

    send help

--- src/hello/#bored.py
+++ src/hello/#bored.py
@@ -3,8 +3,8 @@ def gold_rat(x,n):
     if n == 0:
         return(x)
     else:
-        print(1 /(1 + gold_rat(x, n-1)))
-        return()
+        print(1 /(1 + gold_rat(x, n - 1)))
+        return(gold_rat(x, n - 1))
 
 gold_rat(int(input("""
 x value here
