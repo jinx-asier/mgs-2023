e1a75f8122ec1ac0f38a8d06e3fca533548f9615
Jinx Asier <austintalisa@gmail.com>
2023-06-07 16:28:42 +0000

    try to continue

---
+++ src/hello/#bored.py
@@ -0,0 +1,12 @@
+#mess around
+def gold_rat(x,n):
+    if n == 0:
+        return(x)
+    else:
+        print(1 /(1 + gold_rat(x, n-1)))
+        return()
+
+gold_rat(int(input("""
+x value here
+>>>""")), int(input("""
+n value here>>>""")))
