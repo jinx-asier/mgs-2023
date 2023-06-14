2c63e7b16ceec6b227165819e7ce35d262a0a4b5
Jinx Asier <austintalisa@gmail.com>
2023-06-14 16:00:42 +0000

    log

--- src/hello/#bored.py
+++ src/hello/#bored.py
@@ -1,12 +1,28 @@
-#mess around
-def gold_rat(x,n):
-    if n == 0:
-        return(x)
-    else:
-        print(1 /(1 + gold_rat(x, n - 1)))
-        return(gold_rat(x, n - 1))
 
-gold_rat(int(input("""
-x value here
->>>""")), int(input("""
-n value here>>>""")))
+def clock(b, p, e, op):
+    if p == 0:
+        return e
+    elif p == 1:
+        return b
+    else:
+        return op(b, clock(b, p-1, e, op))
+
+
+def concat_str(str1, str2):
+    return str1 + str2
+
+
+#print(clock("hello-", 6, "", concat_str))
+
+
+def monoid_fast_power(b,p,e,op):
+    if p == 0:
+        return e
+    elif p == 1:
+        return b
+    elif p % 2 == 0:    #even
+        return monoid_fast_power(op(b,b), p//2,e,op)
+    else:               #odd
+        return op(b,monoid_fast_power(b,p-1,e,op))
+
+print(monoid_fast_power("hello-", 103, "", concat_str))
